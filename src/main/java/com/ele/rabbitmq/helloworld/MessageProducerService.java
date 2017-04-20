8dd36286615afc2c53df96d0f956143a6e6212bc
wangchongbei <wchb20155@gmail.com>
2017-04-20 21:44:27 +0800

    update

--- src/main/java/com/ele/rabbitmq/helloworld/MessageProducerService.java
+++ src/main/java/com/ele/rabbitmq/helloworld/MessageProducerService.java
@@ -1,6 +1,5 @@
 package com.ele.rabbitmq.helloworld;
 
-import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -8,9 +7,6 @@ public class MessageProducerService {
 
     @Autowired
     private AmqpTemplate amqpTemplate;
-
-    @Autowired
-    private AmqpAdmin admin;
 
     public void pushToMessageQueue(String routingKey, String message) {
         amqpTemplate.convertAndSend(routingKey, message);
