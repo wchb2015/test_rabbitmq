961e072145dfa4394ebc71d21672836ef67217b0
wangchongbei <wchb20155@gmail.com>
2017-04-20 21:32:39 +0800

    update

--- src/main/java/com/ele/rabbitmq/helloworld/MessageProducerService.java
+++ src/main/java/com/ele/rabbitmq/helloworld/MessageProducerService.java
@@ -1,17 +1,16 @@
 package com.ele.rabbitmq.helloworld;
 
+import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.core.AmqpTemplate;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
-@Service
 public class MessageProducerService {
 
     @Autowired
     private AmqpTemplate amqpTemplate;
+
     @Autowired
-    private ConnectionFactory connectionFactory;
+    private AmqpAdmin admin;
 
     public void pushToMessageQueue(String routingKey, String message) {
         amqpTemplate.convertAndSend(routingKey, message);
--- src/test/java/com/ele/rabbitmq/helloworld/MessageServiceTest.java
+++ src/test/java/com/ele/rabbitmq/helloworld/MessageServiceTest.java
@@ -1,6 +1,5 @@
 package com.ele.rabbitmq.helloworld;
 
-import com.ele.rabbitmq.comsumer.MessageConsumerService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,28 +7,18 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("classpath:spring/spring-mq.xml")
+@ContextConfiguration("classpath:spring/application-context.xml")
 public class MessageServiceTest {
 
-    private String routingKey;
-    private String message;
-
-    @Autowired
-    private MessageConsumerService consumerService;
+    private String routingKey = "rabbit_queue_one_0420";
+    private String message = "这个消息哈哈......AAAAA";
 
     @Autowired
     private MessageProducerService producerService;
 
     @Test
     public void pushToMessageQueue() {
-        routingKey = "test_queue_1";
-        message = "Test ";
         producerService.pushToMessageQueue(routingKey, message);
-    }
-
-    @Test
-    public void popMessage() throws Exception {
-        consumerService.popMessage("test_queue_1");
     }
 
 }
