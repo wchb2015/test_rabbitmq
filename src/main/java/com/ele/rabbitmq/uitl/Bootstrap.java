d28b97813f9103a48c662cef15ce84fe9b0b7ca2
wangchongbei <wchb20155@gmail.com>
2017-05-08 18:04:43 +0800

    update

--- src/main/java/com/ele/rabbitmq/uitl/Bootstrap.java
+++ src/main/java/com/ele/rabbitmq/uitl/Bootstrap.java
@@ -3,8 +3,11 @@ package com.ele.rabbitmq.uitl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import java.util.Arrays;
 
 public class Bootstrap {
+
+    public static ClassPathXmlApplicationContext context = null;
 
     private static Logger LOG = LoggerFactory.getLogger(Bootstrap.class);
 
@@ -12,10 +15,11 @@ public class Bootstrap {
         try {
             if (args == null || args.length == 0) {
                 LOG.info("spring server bootstrap use config file : application-context.xml under the classpath.");
-                new ClassPathXmlApplicationContext("classpath:spring/application-context.xml");
+                context = new ClassPathXmlApplicationContext("classpath:spring/application-context.xml");
             } else {
                 new ClassPathXmlApplicationContext(args);
             }
+            LOG.info("BeanDefinitionNames:{}", Arrays.asList(context.getBeanDefinitionNames()));
         } catch (Throwable t) {
             LOG.error("Throwable Occurs in Bootstrap!", t);
             System.exit(-1);
